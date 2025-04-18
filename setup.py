eda58e57e07a5b637737c1071f4120986eb1996b
wc-bikram <bikramroy9991@gmail.com>
2025-04-19 00:57:26 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI project',
+    version = '0.0.0',
+    author= 'Bikram Roy',
+    author_email= 'bikramroy756@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+    
+)
